d4f636dedd30e8ebaadd92210af43022267acebd
Jorge Aguiniga <jorge.aguiniga94@gmail.com>
2017-03-31 17:21:24 -0700

    db and table creation

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -18,16 +18,32 @@ $dbcreate = 'CREATE DATABASE IF NOT EXISTS '.$dbname;
 if ($db->query($dbcreate) === true)
 {
 	echo $dbname." created\n";
-	$db->query('DROP DATABASE '.$dbname);
+	//$db->query('DROP DATABASE '.$dbname);
+    $db->select_db($dbname);
 }
 else
 {
 	echo "Database could not be created: ".$db->error."\n";
 }
 
-$dbtables[4]= ["List(id INT(6) UNSIGNED AUTO INCREMENT PRIMARY Key, name VARCHAR(100))",
-			   "List Relationship(parent_id INT(6), child_id INT(6)"];
+$dbtables = ["CREATE TABLE `list` (`list_id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `list_name` VARCHAR(100) NOT NULL, PRIMARY KEY (`list_id`));",
+  "CREATE TABLE `list_relationship` ( `parent_id` INT NOT NULL, `child_id` INT NOT NULL);",
+  "CREATE TABLE `note_relationship` ( `list_id` INT NOT NULL, `note_id` INT NOT NULL);",
+  "CREATE TABLE `note` ( `note_id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `note_name` VARCHAR(100) NOT NULL, `note_date` DATE NOT NULL, `note_description` VARCHAR(500) NOT NULL, PRIMARY KEY (`note_id`));"];
 
+foreach ($dbtables as $query) {
+    if ($db->query($query) === true)
+    {
+        echo $query." created\n\n";
+    }
+    else
+    {
+        echo "Table could not be created: ".$db->error."\n";
+    }
+}
 
 $db->close();
+
+
+
 
